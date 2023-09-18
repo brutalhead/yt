6e8e063d98f641cfce68edece0bcad2b4cd7d11b
brutalhead <144815214+brutalhead@users.noreply.github.com>
2023-09-18 11:40:58 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -139,4 +139,3 @@ sr.reveal('.features__map', { delay: 600, origin: 'bottom' });
 sr.reveal('.features__card', {interval: 300});
 sr.reveal('.featured__card', {interval: 100});
 sr.reveal('.featured__card, .logos__content, .footer__content', {interval: 100});
-sr.reveal('.footer__copy', { delay: 600, origin: 'bottom' });
